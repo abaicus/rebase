9354d87810da264e04faea40bfb540845da82855
abaicus <baicusandrei@me.com>
2020-12-06 14:25:34 +0200

    add files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('Starting Rebase')
 
-import {context} from "@actions/github";
+const github = require('@actions/github');
 
-const payload = JSON.stringify(context, undefined, 2)
+const payload = JSON.stringify(github.context, undefined, 2)
 console.log(`The event context: ${payload}`);
